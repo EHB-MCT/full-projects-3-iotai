593ce5f7fee2b4c0ffb061c8e2a1fe34e93ed565
YoranAsselman <91253305+YoranAsselman@users.noreply.github.com>
2023-01-26 10:21:44 +0100

    Revert "Dev"

--- js/laadscherm.js
+++
@@ -1,7 +0,0 @@
-window.onload = (event) => {
-  var laadInterval = setInterval(call_back, 600000);
-  function call_back() {
-     document.location.href = "welcome.html";
-     clearInterval(laadInterval);
-  }
-  };
