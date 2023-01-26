0b3a570ccbd9625f58c295afa6a29f09e03a1812
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2023-01-27 00:40:31 +0100

    [DONE] Screens: join lobby, lobby 1,2,3 + HTML/CSS/JS

--- js/header.js
+++ js/header.js
@@ -17,11 +17,5 @@ function renderHeader() {
 
     document.body.prepend(header);
 
-    const infoElement = document.createElement('img');
-    infoElement.src = '../assets/icons/info-circle.png';
-    infoElement.style.position = 'absolute';
-    infoElement.style.top = '5px';
-    infoElement.style.right = '10px';
-
-    header.append(logoElement, infoElement);
+    header.append(logoElement);
 }
--- js/join.js
+++ js/join.js
@@ -1,13 +1,45 @@
 'use strict';
 
 window.onload = () => {
+    init();
     makeHomeActive();
 };
 
 function makeHomeActive() {
     const home = document.querySelector('#home-img');
-    console.log(home);
     home.src = '../assets/nav/home-active.png';
     home.width = home.width * 1.5;
     home.height = home.height * 1.5;
+}
+
+function init() {
+    renderName();
+    initJoinLobby();
+}
+
+function renderName() {
+    const name = document.querySelector('#name');
+    fetch('https://iotai-backend.onrender.com/player/1', { method: 'GET' })
+        .then((res) => res.json())
+        .then((data) => {
+            name.textContent = data[0].name;
+        });
+}
+
+function initJoinLobby() {
+    const button = document.querySelector('#join-lobby');
+    button.addEventListener('click', () => {
+        const ic = document.querySelector('#game_pin').value;
+
+        if (!ic) return alert('No invite code');
+        fetch(`https://iotai-backend.onrender.com/lobby/${ic}/join`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ player_id: 1 }),
+        })
+            .then((res) => res.json())
+            .then(() => {
+                window.location = `${window.location.origin}/html/lobby.html`;
+            });
+    });
 }
