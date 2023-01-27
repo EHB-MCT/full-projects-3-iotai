4579e668f19653efb9783e7bda4be610ab70405b
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2023-01-27 16:33:46 +0100

    Trying to fix merge request

--- js/laadscherm.js
+++
@@ -1,7 +0,0 @@
-window.onload = (event) => {
-  var laadInterval = setInterval(call_back, 6000);
-  function call_back() {
-     document.location.href = "html/welcome.html";
-     clearInterval(laadInterval);
-  }
-  };
