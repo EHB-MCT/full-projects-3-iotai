edd320daed42d13109e67ef13530c58340bbb28a
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2023-01-27 10:05:12 +0100

    made profile icon active on profile page

--- js/profilePage.js
+++ js/profilePage.js
@@ -4,4 +4,12 @@ import { renderNav} from './footer.js';
 
 window.onload = async () => {
     renderNav();
+    makeProfileActive();
 };
+
+function makeProfileActive() {
+    const profile = document.querySelector('#profile-img');
+    profile.src = '../assets/nav/profile-active.png';
+    profile.width = profile.width * 1.5;
+    profile.height = profile.height * 1.5;
+}
