cc1b1f5c3256c0f572ac1f317023c09353094572
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2023-01-30 09:43:45 +0100

    [DONE] Browse avatars + update in DB

--- js/join.js
+++ js/join.js
@@ -20,6 +20,14 @@ function makeHomeActive() {
 function init() {
     renderName();
     initJoinLobby();
+    const arrowLeft = document.querySelector('#arrow-left');
+    const arrowRight = document.querySelector('#arrow-right');
+    arrowLeft.addEventListener('click', () => {
+        browseAvatars(-1);
+    });
+    arrowRight.addEventListener('click', () => {
+        browseAvatars(1);
+    });
 }
 
 function renderName() {
@@ -51,5 +59,51 @@ function initJoinLobby() {
                 cookie.setCookie('lobby_invite_code', ic, { 'max-age': timeUntillCookieExpiresInSeconds });
                 window.location = `${window.location.origin}/html/lobby.html`;
             });
+    });
+}
+
+function browseAvatars(direction) {
+    const avatars = {
+        green: [
+            'agate-g',
+            'baby-g',
+            'desert-rose-g',
+            'diamond-g',
+            'diplodocus-g',
+            'fossile-g',
+            'heart-g',
+            'pterosaur-g',
+            'quartz-g',
+            't-rex-g',
+            'tree-g',
+            'waterlily-g',
+        ],
+    };
+
+    let currentAvatar = document.querySelector('#avatar').alt;
+    let nextAvatar = '';
+    const currentAvatarIndex = avatars.green.indexOf(currentAvatar);
+    if (currentAvatarIndex == avatars.green.length - 1 && direction == 1) {
+        nextAvatar = avatars.green[0];
+    } else if (currentAvatarIndex == 0 && direction == -1) {
+        nextAvatar = avatars.green[avatars.green.length - 1];
+    } else if (direction == -1) {
+        nextAvatar = avatars.green[currentAvatarIndex + direction];
+    } else if (direction == 1) {
+        nextAvatar = avatars.green[currentAvatarIndex + direction];
+    }
+    const avatarIMG = document.querySelector('#avatar');
+    avatarIMG.setAttribute('src', `../assets/avatars/avatar-${nextAvatar}.png`);
+    avatarIMG.setAttribute('alt', nextAvatar);
+    console.log(nextAvatar);
+    fetch('https://iotai-backend.onrender.com/player/avatar/update', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            player_id: 1,
+            avatar: nextAvatar,
+        }),
     });
 }
