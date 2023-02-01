743231ab01fa6fd2ec971dea1729713be4f494b7
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2023-02-01 13:46:27 +0100

    Controleren van antwoord (+ alert)

--- js/game.js
+++ js/game.js
@@ -19,19 +19,30 @@ popupBtns.forEach(function (btn) {
         <div class="popup-content">
             <img class="close" src="/assets/icons/cross-icon.png" height="30px" />
             <p class="task-text">${task.description}</p>
-            <img class="task-image" src=/assets/tasks/${task.image}>
+            <img class="task-image" src=/assets/tasks/${task.img}>
             <div class="form">
                 <div class="input">
                     <input required type="text" name="text" autocomplete="off" placeholder="Answer" class="input" id="answerInput" />
                 </div>
                 <div class="buttons">
-                    <button class="popup-button glow_blue green">Submit</button>
+                    <button class="popup-button glow_blue green" id="submit-answer-button">Submit</button>
                 </div>
             </div>
         </div>
         </div>
         `;
         document.body.appendChild(popup);
+
+        const submitAnswerButton = document.getElementById('submit-answer-button');
+        submitAnswerButton.onclick = function () {
+            const answerInput = document.getElementById('answerInput');
+            const answer = answerInput.value;
+            if (answer === task.answer) {
+            alert('Correct answer!');
+            } else {
+            alert('Incorrect answer, please try again.');
+            }
+        };
 
         const close = popup.getElementsByClassName('close')[0];
         close.onclick = function () {
