2d5ce6809195ff75ad93f3268f5e37ed2a164bd0
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2023-02-01 11:46:50 +0100

    The right task is displayed when clicking on task button (connected to ID)

--- js/game.js
+++ js/game.js
@@ -4,25 +4,35 @@ const popupBtns = document.querySelectorAll('.popupBtn');
 
 popupBtns.forEach(function (btn) {
   btn.onclick = function () {
-    const popup = document.createElement('div');
     const taskId = btn.getAttribute('data-task-id');
-    console.log(taskId);
-    popup.innerHTML = `
-    <div id="popup" style="display: block">
-    <div class="popup-content">
-        <img class="close" src="/assets/icons/cross-icon.png" height="30px" />
-        <p class="task-text">Vraag</p>
-        <button class="popup-button glow_blue green">Submit</button>
-    </div>
-    </div>
-    `;
-    document.body.appendChild(popup);
+    const popup = document.createElement('div');
+    popup.id = 'popup';
+    popup.style.display = 'block';
 
-    const close = popup.getElementsByClassName('close')[0];
-    close.onclick = function () {
-      popup.style.display = 'none';
-      popup.remove();
-    };
+    // Call the API to retrieve the task description
+    const query = "SELECT * FROM tasks WHERE id = 1";
+    fetch(`https://iotai-backend.onrender.com/tasks?query=${query}`)
+      .then(response => response.json())
+      .then(tasks => {
+        const task = tasks.find(t => t.id === Number(taskId));
+        console.log(task.description);
+        popup.innerHTML = `
+        <div id="popup" style="display: block">
+        <div class="popup-content">
+            <img class="close" src="/assets/icons/cross-icon.png" height="30px" />
+            <p class="task-text">${task.description}</p>
+            <button class="popup-button glow_blue green">Submit</button>
+        </div>
+        </div>
+        `;
+        document.body.appendChild(popup);
+
+        const close = popup.getElementsByClassName('close')[0];
+        close.onclick = function () {
+          popup.style.display = 'none';
+          popup.remove();
+        };
+      });
   };
 });
 
