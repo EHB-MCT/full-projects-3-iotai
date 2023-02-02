6000a942eb244aa5a2975c00c60db3d56b4779b4
YoranAsselman <yoran.asselman@student.ehb.be>
2023-02-02 13:46:44 +0100

    fixed bugs

--- js/voting.js
+++ js/voting.js
@@ -3,16 +3,17 @@
 import * as cookie from './cookie.js';
 
 window.onload = async () => {
-    addPlayers();
+    await addPlayers();
+    initVoteButton();
 };
 
 /*spelers inladen*/
-function addPlayers() {
+async function addPlayers() {
     const votingContainer = document.querySelector('#voting-system');
     cookie.setCookie('lobby_ic', 'GNHNTR');
     console.log(document.cookie);
     const lobby_ic = cookie.getCookie('lobby_ic');
-    fetch(`https://iotai-backend.onrender.com/lobby/${lobby_ic}`, {
+    await fetch(`https://iotai-backend.onrender.com/lobby/${lobby_ic}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -20,13 +21,13 @@ function addPlayers() {
     })
         .then((res) => res.json())
         .then((lobby) => {
-            lobby.players.forEach((player) => {
+            lobby.players.forEach((player, index) => {
                 votingContainer.innerHTML += `
             <div class="votingContainer">
             <p>${player.name}</p>
             <div class="av-vote">
             <img id="avatar-img" src="../assets/avatars/avatar-${player.avatar}.png">
-            <button class="btn-vote button-green" id="btn1">Vote</button>
+            <button class="btn-vote button-green" id="btn${index}">Vote</button>
             </div>
             </div>
             `;
@@ -34,28 +35,33 @@ function addPlayers() {
         });
 }
 
-/*pressing vote button*/
-const buttons = document.querySelectorAll('.btn-vote');
+async function initVoteButton() {
+    /*pressing vote button*/
+    const voteBTNs = document.querySelectorAll('.btn-vote');
+    const skipVoteBTN = document.querySelector('#btn-skip-vote');
+    let hasVoted = false;
 
-buttons.forEach((button) => {
-    button.addEventListener('click', function () {
-        buttons.forEach((b) => {
-            b.classList.remove('button-yellow');
-            if (b.id == 'btn-vote') {
-                b.innerHTML = 'Vote skip';
-            } else {
-                b.innerHTML = 'Vote';
-            }
-            voted();
+    voteBTNs.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (hasVoted == true) return;
+            button.innerHTML = 'Voted';
+            // vote fetch api
+            button.classList.add('button-yellow');
+
+            // Update has voted state
+            hasVoted = true;
         });
-        this.classList.add('button-yellow');
-        if (this.id == 'btn-vote') {
-            this.innerHTML = 'Voted skip';
-        } else {
-            this.innerHTML = 'Voted';
-        }
     });
-});
+    skipVoteBTN.addEventListener('click', () => {
+        if (hasVoted == true) return;
+        skipVoteBTN.innerHTML = 'Skipped vote';
+        // vote skip fetch api
+        skipVoteBTN.classList.add('button-yellow');
+
+        // Update has voted state
+        hasVoted = true;
+    });
+}
 
 /*show amount of votes*/
 var votes = 0;
@@ -76,9 +82,10 @@ var redirect = setInterval(function () {
 }, 1000);
 
 async function endMeeting() {
+    const lobby_ic = cookie.getCookie('lobby_invite_code');
     fetch(`https://iotai-backend.onrender.com/lobby/${lobby_ic}/end-meeting`)
         .then((res) => res.json())
-        .then((data) => {
+        .then(() => {
             console.log('meeting ended');
             /*const progress = document.querySelector('#progress');
             progress.style.width = `${data.progress}%`;*/
